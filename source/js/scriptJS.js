fec34336d56d8712a26b48c5dd91039acbf268ba
lívia <livcristinna@gmail.com>
2021-10-08 15:21:39 -0300

    abstract expansive

--- source/js/scriptJS.js
+++ source/js/scriptJS.js
@@ -1,1 +1,15 @@
 console.log('Hello teste Galoá');
+function readMore() {
+    let elip = document.getElementById('elipsis');
+    let moreRead = document.getElementById("more");
+    let abstract = document.getElementById("content-abstract");
+    let butRead = document.getElementById("buttonRead");
+
+    if(elip.style.display != "none"){
+       
+        moreRead.style.display = "inline";
+        abstract.style.height = "500px";
+        buttonRead.style.display = "none";
+    }
+
+}
