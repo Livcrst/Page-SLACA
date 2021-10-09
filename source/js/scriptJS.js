629b76782bd8efae6a27c9e8662494f539d9024c
lívia <livcristinna@gmail.com>
2021-10-09 13:16:35 -0300

    cards for replies

--- source/js/scriptJS.js
+++ source/js/scriptJS.js
@@ -26,18 +26,8 @@ const AddTopic = ()=>{
 
 };
 
-const SendTopic = () => {
-    let sendTopic = document.getElementById("topic-send");
-    // let sendButton = document.getElementById('send');
-
-    // Criar a tela de enviado para o card 
-    if (sendTopic.style.display =! "none"){
-        sendTopic.style.display = "none";
-    }
-    else{
-        sendTopic.style.display = "inline";
-
-    }
-
-};
+const Replies = () => {
+    let replyDiscussions = document.querySelectorAll("[reply]");
+    console.log(replyDiscussions)
+}
 
