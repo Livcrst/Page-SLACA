2343082f565684a9521707dfa2b1fa46d4722b8c
lívia <livcristinna@gmail.com>
2021-10-08 21:08:20 -0300

    create add topic

--- source/js/scriptJS.js
+++ source/js/scriptJS.js
@@ -9,7 +9,20 @@ function readMore() {
        
         moreRead.style.display = "inline";
         abstract.style.height = "500px";
-        buttonRead.style.display = "none";
+        butRead.style.display = "none";
     }
 
+};
+
+const AddTopic = ()=>{
+    let newTopic = document.getElementById("new-topic");
+    let shareTopic = document.getElementById("share");
+
+    if (shareTopic.style.display != "none"){
+        shareTopic.style.display = "none";
+        newTopic.style.display = "inline";
+    }
+
+
 }
+
